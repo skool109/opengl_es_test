57a94a4fc4477faa975c84175a5071d7db92cac0
Yun <skool109@naver.com>
2020-05-12 23:49:58 +0900

    2020/05/12 commit

--- app/src/main/java/com/example/opengl_test/OpenGLRenderer.java
+++ app/src/main/java/com/example/opengl_test/OpenGLRenderer.java
@@ -2,11 +2,56 @@ package com.example.opengl_test;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL;
 import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle mTriangle; // 0512
+
+    public static int loadShader(int type, String shaderCode) {
+        // 빈 쉐이더를 생성하고 그 인덱스를 할당.
+        int shader = GLES20.glCreateShader(type);
+
+        // *컴파일 결과를 받을 공간을 생성.
+        IntBuffer compiled = ByteBuffer.allocateDirect(4).order(ByteOrder.nativeOrder()).asIntBuffer();
+        String shaderType;
+
+        // *컴파일 결과를 출력하기 위해 쉐이더를 구분.
+        if(type == GLES20.GL_VERTEX_SHADER)
+            shaderType = "Vertex";
+        else if(type == GLES20.GL_FRAGMENT_SHADER)
+            shaderType = "Fragment";
+        else
+            shaderType = "Unknown";
+
+        // 빈 쉐이더에 소스코드를 할당.
+        GLES20.glShaderSource(shader, shaderCode);
+        // 쉐이더에 저장 된 소스코드를 컴파일
+        GLES20.glCompileShader(shader);
+
+        // *컴파일 결과 오류가 발생했는지를 확인.
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled);
+        // *컴파일 에러가 발생했을 경우 이를 출력.
+        if(compiled.get(0) == 0) {
+            GLES20.glGetShaderiv(shader,GLES20.GL_INFO_LOG_LENGTH,compiled);
+            if (compiled.get(0) > 1){
+                Log.e("Shader", shaderType + " shader: " + GLES20.glGetShaderInfoLog(shader));
+            }
+            GLES20.glDeleteShader(shader);
+            Log.e("Shader", shaderType + " shader compile error.");
+        }
+
+        // 완성된 쉐이더의 인덱스를 리턴.
+        return shader;
+    }
 
     // GLSurfaceView가 생성되었을 때 한번 호출되는 메소드
     // OpenGL 환경 설정, 그래픽 객체 초기화 등 처리 시 사용
@@ -15,6 +60,8 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         // color buffer 클리어 시 사용할 색 지정
         // red, green, blue, alpha 순 (0~1)
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        mTriangle = new Triangle();
     }
 
     // GLSurfaceView가 다시 그려질 때마다 호출되는 메소드
@@ -25,6 +72,8 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         // depth buffer (GL_DEPTH_BUFFER_BIT)
         // stencil buffer (GL_STENCIL_BUFFER_BIT)
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        mTriangle.draw();
     }
 
     // GLSurfaceView 크기 변경, 디바이스 화면 방향 전환으로 geometry가 바뀔 때 호출되는 메소드
---
+++ app/src/main/java/com/example/opengl_test/Triangle.java
@@ -0,0 +1,93 @@
+package com.example.opengl_test;
+
+import android.opengl.GLES20;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+public class Triangle {
+
+    private final String vertexShaderCode =
+            "#version 300 es                            \n" +
+                    "layout (location = 0) in vec4 vPosition;   \n" +
+                    "void main() {                              \n" +
+                    "   gl_Position = vPosition;                \n" +
+                    "}                                          \n";
+
+    private final String fragmentShaderCode =
+            "#version 300 es                            \n" +
+                    "precision mediump float;                   \n" +
+                    "uniform vec4 vColor;                       \n" +
+                    "out vec4 fragColor;                        \n" +
+                    "void main() {                              \n" +
+                    "  fragColor = vColor;                      \n" +
+                    "}                                          \n";
+
+    private FloatBuffer vertexBuffer;
+
+    static final int COORDS_PER_VERTEX = 3; // x, y, z
+    static float triangleCoords[] = {       // 반시계 방향으로 좌표 설정.
+            0.0f,  0.622008459f, 0.0f,      // 상단
+            -0.5f, -0.311004243f, 0.0f,     // 좌측하단
+            0.5f, -0.311004243f, 0.0f       // 우측하단
+    };
+
+    // color 설정
+    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+
+    private final int mProgram;
+
+    public Triangle() {
+        // 좌표 배열의 수에 해당하는 만큼 버퍼를 할당
+        ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+
+        // 디바이스 하드웨어에서 사용하는 byte order를 사용.
+        bb.order(ByteOrder.nativeOrder());
+
+        // ByteBuffer를 통해 floating point 버퍼를 생성.
+        vertexBuffer = bb.asFloatBuffer();
+        // 이 FloatBuffer에 좌표를 추가.
+        vertexBuffer.put(triangleCoords);
+        // 버퍼 상의 첫 번째 좌표를 읽을 수 있도록 설정.
+        vertexBuffer.position(0);
+
+        // 쉐이더 로드 및 컴파일.
+        int vertexShader = OpenGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = OpenGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        // 프로그램 생성.
+        mProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        GLES20.glLinkProgram(mProgram);
+    }
+
+    private int mPositionHandle;
+    private int mColorHandle;
+
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4;
+
+    public void draw() {
+        // 쉐이더가 포함 된 프로그램을 사용.
+        GLES20.glUseProgram(mProgram);
+
+        // 프로그램에 포함 된 쉐이더에서 vPosition 변수에 접근할 수 있는 핸들을 가져온다.
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        // 해당 값에 대한 Vertex Attribute Array를 활성화한다.
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        // 활성화된 Vertex Attribute Array가 정점 데이터를 가리키도록 한다. (그래픽 장치로 복사)
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+        // vColor 변수에 접근할 수 있는 핸들을 가져온다.
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        // 해당 핸들을 이용해 해당 uniform 변수에 값을 할당한다.
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // 위에서 설정한 값들을 바탕으로 렌더링을 수행한다.
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // 활성화된 Vertex Attribute Array를 비활성화한다.
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+}
