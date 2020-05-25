0b6c893dbd373f2ded543e1086a140dbd5b61931
Yun <skool109@naver.com>
2020-05-25 19:22:31 +0900

    2020/05/24 commit

--- app/src/main/java/com/example/opengl_test/HomeActivity.java
+++ app/src/main/java/com/example/opengl_test/HomeActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -42,6 +50,25 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "보정후 데이터를 시각화합니다.", Toast.LENGTH_LONG).show(); // 출력문구
                 Intent intent = new Intent(getApplicationContext(), Main2Activity.class); // opengl 화면으로 전환
                 startActivity(intent);
+            }
+        });
+
+        final TextView text_test= findViewById(R.id.text_test);
+        Button btn_test = findViewById(R.id.btn_test);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    CSVReader reader = new CSVReader(new InputStreamReader(getAssets().open("test.csv")));
+                    String[] nextLine;
+                    while ((nextLine = reader.readNext()) != null) {
+                        // nextLine[] is an array of values from the line
+                        System.out.println(nextLine[0] + " , " + nextLine[1] + " , " + nextLine[2]);
+                        text_test.setText("CSV file read complete");
+                    }
+                } catch (IOException e) {
+
+                }
             }
         });
     }
